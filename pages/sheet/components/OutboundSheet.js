693b78db9e60954d863741394a759d053251659c
ll-fox <657609759@qq.com>
2023-01-12 21:15:20 +0800

    fix: 添加单位

--- pages/sheet/components/OutboundSheet.js
+++ pages/sheet/components/OutboundSheet.js
@@ -205,7 +205,8 @@ export default function OutboundSheet() {
     if (!/^(0|[1-9]\d*)(\.\d+)?$/.test(n)){
         return "数据非法";  //判断数据是否大于0
     }
-    var unit = "千百拾亿千百拾万千百拾元角分", str = "";
+    var unit = '仟佰拾亿仟佰拾万仟佰拾元角分',
+      str = ''
     n += "00";  
     var indexpoint = n.indexOf('.');  // 如果是小数，截取小数点前面的位数
     if (indexpoint >= 0){
@@ -215,7 +216,13 @@ export default function OutboundSheet() {
     for (var i=0; i < n.length; i++){
         str += "零壹贰叁肆伍陆柒捌玖".charAt(n.charAt(i)) + unit.charAt(i);  //遍历转化为大写的数字
     }
-    return str.replace(/零(千|百|拾|角)/g, "零").replace(/(零)+/g, "零").replace(/零(万|亿|元)/g, "$1").replace(/(亿)万|壹(拾)/g, "$1$2").replace(/^元零?|零分/g, "").replace(/元$/g, "元整"); // 替换掉数字里面的零字符，得到结果
+    return str
+      .replace(/零(仟|佰|拾|角)/g, '零')
+      .replace(/(零)+/g, '零')
+      .replace(/零(万|亿|元)/g, '$1')
+      .replace(/(亿)万|壹(拾)/g, '$1$2')
+      .replace(/^元零?|零分/g, '')
+      .replace(/元$/g, '元整') // 替换掉数字里面的零字符，得到结果
   }
 
   return (
